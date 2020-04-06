c584c156dbe54384df5cad5a5d035aab79f83278
Alyson Xavier Leite <alyson.nextage@gmail.com>
2020-04-05 23:55:38 -0300

    Novo paredão

--- run-server.bat
+++ run-server.bat
@@ -1,5 +1,5 @@
 SET /P GLOBO_USERNAME= Digite o e-mail da Globo:
 SET /P GLOBO_PASSWORD= Digite a senha da Globo:
-SET /P PARTICIPANTE= Digite o participante que vc quer votar [babu, gabi ou thelma]:
+SET /P PARTICIPANTE= Digite o participante que vc quer votar [babu, flay ou marcela]:
 
 node ./src/vote.js %voto% %email% %senha%
--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 const links = {
-  voteURL: 'https://gshow.globo.com/realities/bbb/bbb20/votacao/paredao-bbb20-quem-voce-quer-eliminar-babu-gabi-ou-thelma-305135b8-b442-4cc8-888f-2a01ed79cc2d.ghtml',
+  voteURL: 'https://gshow.globo.com/realities/bbb/bbb20/votacao/paredao-bbb20-quem-voce-quer-eliminar-babu-flayslane-ou-marcela-5ed83d00-014e-401d-80c8-20314769ce2f.ghtml',
   globoLoginURL: "https://minhaconta.globo.com",
   captchaURL: "https://captcha.globo.com/api/challenge/generate",
   challengeAcceptedURL: "https://royale.globo.com/polls/",
@@ -11,13 +11,13 @@ const getPosition = name => {
   switch (name) {
     case 'babu':
       return 1;
-    case 'gabi':
+    case 'flay':
       return 2;
-    case 'thelma':
+    case 'marcela':
       return 3;
   }
 
-  throw new Error('candidato invalido, use babu | gabi | thelma');
+  throw new Error('candidato invalido, use babu | flay | marcela');
 };
 
 
@@ -25,7 +25,7 @@ const config = {
   participantPosition: getPosition(process.argv[2] || process.env.PARTICIPANTE),  // [1,2, 3] are the possible options.
   timeoutClick: 5 * 1000,  // in MS
   waitClick: 2 * 1000, // in milisseconds
-  captchaCenter: 30,
+  captchaCenter: 28,
   captchaIndividualSize: 53,
 };
 
